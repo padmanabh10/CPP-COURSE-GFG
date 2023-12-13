e1e3717da6bedf3bd7e64b0434782ed2c25b06ca
Padmanabh Kulkarni <officialpadmanabh@gmail.com>
2023-12-13 18:38:51 +0530

    gfg

---
+++ TUT2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age=34;
+    string name="Sandeep";
+    char gender='m';
+    bool isMarried= true;
+    float weight= 68;
+    cout<<name<<endl<<age<<endl<<gender<<endl<<isMarried<<endl<<weight;
+}
+
