1e530a708eadb8d2c9800f3582671f894006e78f
Padmanabh Kulkarni <officialpadmanabh@gmail.com>
2023-12-18 20:33:09 +0530

    gfg

--- trianglePattern.cpp
+++ trianglePattern.cpp
@@ -10,9 +10,13 @@ int main()
     cin>>n;
     for(int i=0;i<n;i++)
     {
-        for(int j=0;j<=i;j++)
+        for(int j=1;j<=n;j++)
         {
-            cout<<"*";
+            if (j<n-i)
+                cout<<" ";
+            else
+                cout<<"*";
+
         }
         cout<<endl;
 
