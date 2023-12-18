ec9c2ad0408abd8835cebb38a1d7df1fac6321af
Padmanabh Kulkarni <officialpadmanabh@gmail.com>
2023-12-18 20:20:53 +0530

    gfg

---
+++ trianglePattern.cpp
@@ -0,0 +1,21 @@
+//
+// Created by ppkkv on 18-12-2023.
+//
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"Enter a number:";
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+
+    }
+
+}
