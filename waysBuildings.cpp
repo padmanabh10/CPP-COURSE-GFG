108ec8a23fcad864759897072795e5863cb10e95
ppkkv <officialpadmanabh@gmail.com>
2024-01-05 22:21:36 +0530

    gfg

---
+++ waysBuildings.cpp
@@ -0,0 +1,28 @@
+//
+// Created by ppkkv on 05-01-2024.
+//
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+    const long long mod = 1000000007;
+
+    int TotalWays(int N) {
+        long long a = 1, b = 1, c = 0;
+        for (int i = 1; i <= N; i++) {
+            c = (a % mod + b % mod) % mod;
+            a = b % mod;
+            b = c % mod;
+        }
+        return static_cast<int>((c * c) % mod);
+    }
+};
+int main(){
+    int N;
+    cout<<"Enter No Plots on each side:\n";
+    cin >> N;
+    Solution ob;
+    int ans = ob.TotalWays(N);
+    cout <<"Total no of possible ways of construction:" << ans <<"\n";
+    return 0;
+}
