b6ee297de16483956dd2d96c6e9b8e1248c57eb9
ppkkv <officialpadmanabh@gmail.com>
2024-03-20 22:30:37 +0530

    gfg

---
+++ sumOfLongestBloodline.cpp
@@ -0,0 +1,136 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void printInorder(Node *node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    printInorder(node->left);
+    cout << node->data << " ";
+    printInorder(node->right);
+}
+Node *buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size())
+    {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N")
+        {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N")
+        {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+
+    pair<int,int> solve(Node* root)
+    {
+        if(root==NULL){
+            return {0,0};
+        }
+        pair<int,int> a=solve(root->left);
+        pair<int,int> b=solve(root->right);
+
+        if(a.first>b.first){
+            return {a.first+1,a.second+root->data};
+        }else if(b.first>a.first){
+            return {b.first+1,b.second+root->data};
+        }else{
+            return {a.first+1,max(a.second,b.second)+root->data};
+        }
+    }
+    int sumOfLongRootToLeafPath(Node *root)
+    {
+        return solve(root).second;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    cin.ignore();
+        string treeString;
+        getline(cin, treeString);
+        Node *root = buildTree(treeString);
+        Solution obj;
+        int res = obj.sumOfLongRootToLeafPath(root);
+        cout << res << "\n";
+
+    return 0;
+}
